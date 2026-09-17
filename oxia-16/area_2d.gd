725c3d904304003f6179ab5503df295421e11b58
ervis07 <erviservus@gmail.com>
2026-09-17 16:25:31 +0700

    ss

--- oxia-16/area_2d.gd
+++ oxia-16/area_2d.gd
@@ -11,4 +11,4 @@ func _on_body_exited(body: PhysicsBody2D):
 func _process(delta):
 	if entered == true:
 		if Input.is_action_just_pressed("ui_accept"):
-			get_tree().change_scene_to_file("res://naikK.tscn")
+			get_tree().change_scene_to_file("res://rt_1.tscn")
